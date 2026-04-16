620684cf1150ca253e172ba504addf8713933fbd
user <gjxodb1458@gmail.com>
2026-04-16 16:17:14 +0900

    파일 색상처리 구

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -66,10 +66,12 @@
             // panel1
             // 
             panel1.Controls.Add(lvFilesLeft);
-            panel1.Location = new Point(5, 129);
-            panel1.Margin = new Padding(2, 2, 2, 2);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 126);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(320, 279);
+            panel1.Padding = new Padding(5);
+            panel1.Size = new Size(337, 289);
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
             // 
@@ -78,10 +80,10 @@
             lvFilesLeft.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
             lvFilesLeft.FullRowSelect = true;
             lvFilesLeft.GridLines = true;
-            lvFilesLeft.Location = new Point(7, 40);
-            lvFilesLeft.Margin = new Padding(2, 2, 2, 2);
+            lvFilesLeft.Location = new Point(12, 10);
+            lvFilesLeft.Margin = new Padding(2);
             lvFilesLeft.Name = "lvFilesLeft";
-            lvFilesLeft.Size = new Size(293, 192);
+            lvFilesLeft.Size = new Size(323, 266);
             lvFilesLeft.TabIndex = 9;
             lvFilesLeft.UseCompatibleStateImageBehavior = false;
             lvFilesLeft.View = View.Details;
@@ -103,10 +105,11 @@
             // 
             // btnCopyToRight
             // 
-            btnCopyToRight.Location = new Point(233, 50);
-            btnCopyToRight.Margin = new Padding(2, 2, 2, 2);
+            btnCopyToRight.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnCopyToRight.Location = new Point(256, 50);
+            btnCopyToRight.Margin = new Padding(2);
             btnCopyToRight.Name = "btnCopyToRight";
-            btnCopyToRight.Size = new Size(72, 22);
+            btnCopyToRight.Size = new Size(82, 22);
             btnCopyToRight.TabIndex = 6;
             btnCopyToRight.Text = ">>>";
             btnCopyToRight.UseVisualStyleBackColor = true;
@@ -114,10 +117,11 @@
             // 
             // btnSelectRight
             // 
-            btnSelectRight.Location = new Point(230, 2);
-            btnSelectRight.Margin = new Padding(2, 2, 2, 2);
+            btnSelectRight.Anchor = AnchorStyles.Right;
+            btnSelectRight.Location = new Point(310, 10);
+            btnSelectRight.Margin = new Padding(2);
             btnSelectRight.Name = "btnSelectRight";
-            btnSelectRight.Size = new Size(72, 22);
+            btnSelectRight.Size = new Size(81, 22);
             btnSelectRight.TabIndex = 2;
             btnSelectRight.Text = "폴더선택";
             btnSelectRight.UseVisualStyleBackColor = true;
@@ -125,31 +129,35 @@
             // 
             // txtPathRight
             // 
-            txtPathRight.Location = new Point(10, 3);
-            txtPathRight.Margin = new Padding(2, 2, 2, 2);
+            txtPathRight.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            txtPathRight.Location = new Point(10, 9);
+            txtPathRight.Margin = new Padding(2);
+            txtPathRight.Multiline = true;
             txtPathRight.Name = "txtPathRight";
-            txtPathRight.Size = new Size(216, 23);
+            txtPathRight.Size = new Size(296, 23);
             txtPathRight.TabIndex = 1;
             txtPathRight.TextChanged += textBox1_TextChanged;
             // 
             // lblTitle
             // 
             lblTitle.AutoSize = true;
-            lblTitle.Font = new Font("맑은 고딕", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblTitle.Font = new Font("Georgia", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.Green;
             lblTitle.Location = new Point(2, 4);
             lblTitle.Margin = new Padding(2, 0, 2, 0);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(187, 47);
+            lblTitle.Size = new Size(215, 41);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "FileCopare";
             lblTitle.Click += label1_Click;
             // 
             // btnSelectLeft
             // 
-            btnSelectLeft.Location = new Point(233, 6);
-            btnSelectLeft.Margin = new Padding(2, 2, 2, 2);
+            btnSelectLeft.Anchor = AnchorStyles.Right;
+            btnSelectLeft.Location = new Point(256, 8);
+            btnSelectLeft.Margin = new Padding(2);
             btnSelectLeft.Name = "btnSelectLeft";
-            btnSelectLeft.Size = new Size(72, 22);
+            btnSelectLeft.Size = new Size(87, 22);
             btnSelectLeft.TabIndex = 5;
             btnSelectLeft.Text = "폴더선택";
             btnSelectLeft.UseVisualStyleBackColor = true;
@@ -157,16 +165,18 @@
             // 
             // txtPathLeft
             // 
+            txtPathLeft.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             txtPathLeft.Location = new Point(7, 7);
-            txtPathLeft.Margin = new Padding(2, 2, 2, 2);
+            txtPathLeft.Margin = new Padding(2);
             txtPathLeft.Name = "txtPathLeft";
-            txtPathLeft.Size = new Size(204, 23);
+            txtPathLeft.Size = new Size(255, 23);
             txtPathLeft.TabIndex = 2;
             // 
             // btnCopyToLeft
             // 
-            btnCopyToLeft.Location = new Point(2, 50);
-            btnCopyToLeft.Margin = new Padding(2, 2, 2, 2);
+            btnCopyToLeft.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnCopyToLeft.Location = new Point(10, 50);
+            btnCopyToLeft.Margin = new Padding(2);
             btnCopyToLeft.Name = "btnCopyToLeft";
             btnCopyToLeft.Size = new Size(70, 22);
             btnCopyToLeft.TabIndex = 4;
@@ -179,7 +189,7 @@
             splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             splitContainer1.BorderStyle = BorderStyle.FixedSingle;
             splitContainer1.Location = new Point(0, 0);
-            splitContainer1.Margin = new Padding(2, 2, 2, 2);
+            splitContainer1.Margin = new Padding(2);
             splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -195,52 +205,58 @@
             splitContainer1.Panel2.Controls.Add(panel6);
             splitContainer1.Panel2.Controls.Add(panel2);
             splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint_1;
-            splitContainer1.Size = new Size(717, 417);
-            splitContainer1.SplitterDistance = 330;
-            splitContainer1.SplitterWidth = 3;
+            splitContainer1.Size = new Size(737, 417);
+            splitContainer1.SplitterDistance = 339;
+            splitContainer1.SplitterWidth = 10;
             splitContainer1.TabIndex = 8;
             // 
             // panel4
             // 
             panel4.Controls.Add(btnSelectLeft);
             panel4.Controls.Add(txtPathLeft);
-            panel4.Location = new Point(2, 81);
-            panel4.Margin = new Padding(2, 2, 2, 2);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 78);
+            panel4.Margin = new Padding(2);
             panel4.Name = "panel4";
-            panel4.Size = new Size(323, 44);
+            panel4.Size = new Size(337, 44);
             panel4.TabIndex = 1;
             // 
             // panel3
             // 
             panel3.Controls.Add(lblTitle);
             panel3.Controls.Add(btnCopyToRight);
-            panel3.Location = new Point(2, 2);
-            panel3.Margin = new Padding(2, 2, 2, 2);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Margin = new Padding(2);
             panel3.Name = "panel3";
-            panel3.Size = new Size(323, 78);
+            panel3.Size = new Size(337, 78);
             panel3.TabIndex = 0;
             // 
             // panel7
             // 
             panel7.Controls.Add(lvFilesRight);
-            panel7.Location = new Point(2, 129);
-            panel7.Margin = new Padding(2, 2, 2, 2);
+            panel7.Dock = DockStyle.Bottom;
+            panel7.Location = new Point(0, 126);
+            panel7.Margin = new Padding(2);
             panel7.Name = "panel7";
-            panel7.Size = new Size(306, 278);
+            panel7.Padding = new Padding(5);
+            panel7.Size = new Size(386, 289);
             panel7.TabIndex = 11;
             // 
             // lvFilesRight
             // 
+            lvFilesRight.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             lvFilesRight.Columns.AddRange(new ColumnHeader[] { columnHeader4, columnHeader5, columnHeader6 });
             lvFilesRight.FullRowSelect = true;
             lvFilesRight.GridLines = true;
-            lvFilesRight.Location = new Point(7, 40);
-            lvFilesRight.Margin = new Padding(2, 2, 2, 2);
+            lvFilesRight.Location = new Point(12, 10);
+            lvFilesRight.Margin = new Padding(2);
             lvFilesRight.Name = "lvFilesRight";
-            lvFilesRight.Size = new Size(300, 192);
+            lvFilesRight.Size = new Size(372, 274);
             lvFilesRight.TabIndex = 9;
             lvFilesRight.UseCompatibleStateImageBehavior = false;
             lvFilesRight.View = View.Details;
+            lvFilesRight.SelectedIndexChanged += lvFilesRight_SelectedIndexChanged;
             // 
             // columnHeader4
             // 
@@ -261,26 +277,28 @@
             // 
             panel6.Controls.Add(btnSelectRight);
             panel6.Controls.Add(txtPathRight);
-            panel6.Location = new Point(2, 81);
-            panel6.Margin = new Padding(2, 2, 2, 2);
+            panel6.Dock = DockStyle.Top;
+            panel6.Location = new Point(0, 78);
+            panel6.Margin = new Padding(2);
             panel6.Name = "panel6";
-            panel6.Size = new Size(306, 44);
+            panel6.Size = new Size(386, 44);
             panel6.TabIndex = 10;
             // 
             // panel2
             // 
             panel2.Controls.Add(panel5);
             panel2.Controls.Add(btnCopyToLeft);
-            panel2.Location = new Point(2, 2);
-            panel2.Margin = new Padding(2, 2, 2, 2);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(306, 78);
+            panel2.Size = new Size(386, 78);
             panel2.TabIndex = 9;
             // 
             // panel5
             // 
             panel5.Location = new Point(2, 76);
-            panel5.Margin = new Padding(2, 2, 2, 2);
+            panel5.Margin = new Padding(2);
             panel5.Name = "panel5";
             panel5.Size = new Size(304, 78);
             panel5.TabIndex = 10;
@@ -289,11 +307,12 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(733, 417);
+            ClientSize = new Size(753, 417);
             Controls.Add(splitContainer1);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             panel1.ResumeLayout(false);
             splitContainer1.Panel1.ResumeLayout(false);
             splitContainer1.Panel2.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -1,39 +1,138 @@
 using System.IO;
+using System.Drawing;
+
 namespace FileCompare
 {
     public partial class Form1 : Form
     {
-        private void FillListView(string folderPath, ListView targetListView)
+        private void CompareAndPopulate()
         {
-            // 1. 혹시 리스트뷰에 예전 데이터가 남아있으면 싹 지워주기 (청소!)
-            targetListView.Items.Clear();
+            // 0. 준비물: 양쪽 경로가 다 있어야 비교를 하것제?
+            string leftPath = txtPathLeft.Text;
+            string rightPath = txtPathRight.Text;
 
-            // 2. 만약 폴더 경로가 비어있거나 없는 폴더라면 그냥 돌아가기 (예외 처리)
-            if (!Directory.Exists(folderPath)) return;
+            if (!Directory.Exists(leftPath) || !Directory.Exists(rightPath)) return;
 
-            // 3. 폴더 정보를 가져오기
-            DirectoryInfo di = new DirectoryInfo(folderPath);
+            // 1. 일단 양쪽 리스트뷰 싹 비우고 시작하쇼!
+            lvFilesLeft.Items.Clear();
+            lvFilesRight.Items.Clear();
 
-            // 4. 폴더 안의 모든 파일을 하나씩 꺼내서 반복하기
-            foreach (FileInfo file in di.GetFiles())
+            // 2. 양쪽 폴더의 파일 목록을 싹 긁어오기
+            DirectoryInfo leftDir = new DirectoryInfo(leftPath);
+            DirectoryInfo rightDir = new DirectoryInfo(rightPath);
+
+            FileInfo[] leftFiles = leftDir.GetFiles();
+            FileInfo[] rightFiles = rightDir.GetFiles();
+
+            // --- [왼쪽 리스트뷰 채우면서 비교하기] ---
+            foreach (var lFile in leftFiles)
             {
-                // [이름] 컬럼에 들어갈 아이템 만들기
-                ListViewItem lvi = new ListViewItem(file.Name);
+                ListViewItem item = new ListViewItem(lFile.Name);
+                item.SubItems.Add((lFile.Length / 1024).ToString("N0") + " KB");
+                item.SubItems.Add(lFile.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
 
-                // [크기] 컬럼에 들어갈 서브 아이템 추가 (Byte를 KB 단위로 변환)
-                long kbSize = file.Length / 1024;
-                lvi.SubItems.Add(kbSize.ToString("N0") + " KB");
+                // 오른쪽 폴더에 똑같은 이름의 파일이 있는지 찾아부러!
+                FileInfo rFile = Array.Find(rightFiles, f => f.Name == lFile.Name);
 
-                // [수정일] 컬럼에 들어갈 서브 아이템 추가
-                lvi.SubItems.Add(file.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                if (rFile == null)
+                {
+                    // [3단계: 단독 파일] - 보라색!
+                    item.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    // [1단계: 비교 기준] - 수정시간 비교 들어가야제!
+                    if (lFile.LastWriteTime > rFile.LastWriteTime)
+                    {
+                        // [2단계: New] - 내가 더 최신이면 빨간색!
+                        item.ForeColor = Color.Red;
+                    }
+                    else if (lFile.LastWriteTime < rFile.LastWriteTime)
+                    {
+                        // [2단계: Old] - 내가 더 옛날이면 회색!
+                        item.ForeColor = Color.Gray;
+                    }
+                    else
+                    {
+                        // [2단계: 동일] - 검은색!
+                        item.ForeColor = Color.Black;
+                    }
+                }
+                lvFilesLeft.Items.Add(item);
+            }
 
-                // 5. 완성된 줄(Item)을 리스트뷰에 딱! 붙여넣기
-                targetListView.Items.Add(lvi);
+            // --- [오른쪽 리스트뷰 채우면서 비교하기] ---
+            foreach (var rFile in rightFiles)
+            {
+                ListViewItem item = new ListViewItem(rFile.Name);
+                item.SubItems.Add((rFile.Length / 1024).ToString("N0") + " KB");
+                item.SubItems.Add(rFile.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                // 이번엔 왼쪽 폴더에 있는지 확인!
+                FileInfo lFile = Array.Find(leftFiles, f => f.Name == rFile.Name);
+
+                if (lFile == null)
+                {
+                    // [3단계: 단독 파일] - 보라색!
+                    item.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    if (rFile.LastWriteTime > lFile.LastWriteTime)
+                    {
+                        // [2단계: New] - 오른쪽이 더 최신이면 빨간색!
+                        item.ForeColor = Color.Red;
+                    }
+                    else if (rFile.LastWriteTime < lFile.LastWriteTime)
+                    {
+                        // [2단계: Old] - 오른쪽이 더 옛날이면 회색!
+                        item.ForeColor = Color.Gray;
+                    }
+                    else
+                    {
+                        item.ForeColor = Color.Black;
+                    }
+                }
+                lvFilesRight.Items.Add(item);
             }
         }
         public Form1()
         {
             InitializeComponent();
+        }
+        private void PopulateListView(string folderPath, ListView targetListView)
+        {
+            // 이제 lvFilesLeft 대신 'targetListView'라는 이름을 써야 만능이 된당게!
+            targetListView.Items.Clear();
+
+            try
+            {
+                // 폴더 훑기
+                foreach (string dirPath in Directory.EnumerateDirectories(folderPath))
+                {
+                    DirectoryInfo di = new DirectoryInfo(dirPath);
+                    ListViewItem item = new ListViewItem(di.Name);
+                    item.SubItems.Add("<Folder>");
+                    item.SubItems.Add(di.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    item.ForeColor = Color.Blue;
+                    targetListView.Items.Add(item); // 전달받은 리스트뷰에 쏙!
+                }
+
+                // 파일 훑기
+                foreach (string filePath in Directory.EnumerateFiles(folderPath))
+                {
+                    FileInfo fi = new FileInfo(filePath);
+                    ListViewItem item = new ListViewItem(fi.Name);
+                    long kbSize = fi.Length / 1024;
+                    item.SubItems.Add(kbSize.ToString("N0") + " KB");
+                    item.SubItems.Add(fi.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    targetListView.Items.Add(item); // 전달받은 리스트뷰에 쏙!
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" 에러!: " + ex.Message);
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -45,14 +144,18 @@ namespace FileCompare
         {
             using (FolderBrowserDialog fbd = new FolderBrowserDialog())
             {
-                // 2. 사용자가 폴더를 고르고 '확인'을 눌렀다면?
                 if (fbd.ShowDialog() == DialogResult.OK)
                 {
-                    // 3. 텍스트박스에 그 폴더 경로를 딱 써주고
+                    txtPathRight.Text = fbd.SelectedPath;
+                    CompareAndPopulate(); // 아따, 인쟈 요놈만 부르면 끝이여!
+                }
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    // 1. 오른쪽 경로 텍스트박스에 주소 딱 찍고!
                     txtPathRight.Text = fbd.SelectedPath;
 
-                    // 4. 2단계에서 만든 함수를 불러서 리스트뷰에 파일을 촤르륵 뿌려줘!
-                    FillListView(fbd.SelectedPath, lvFilesRight);
+                    // 2. ⭐ 요기가 포인트! 이번엔 'lvFilesRight'를 넣어주는 거여!
+                    PopulateListView(fbd.SelectedPath, lvFilesRight);
                 }
             }
         }
@@ -89,8 +192,14 @@ namespace FileCompare
 
         private void button2_Click(object sender, EventArgs e)
         {
+ 
             using (FolderBrowserDialog fbd = new FolderBrowserDialog())
             {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPathLeft.Text = fbd.SelectedPath;
+                    CompareAndPopulate(); // 아따, 인쟈 요놈만 부르면 끝이여!
+                }
                 // 2. 사용자가 폴더를 고르고 '확인'을 눌렀다면?
                 if (fbd.ShowDialog() == DialogResult.OK)
                 {
@@ -98,9 +207,10 @@ namespace FileCompare
                     txtPathLeft.Text = fbd.SelectedPath;
 
                     // 4. 2단계에서 만든 함수를 불러서 리스트뷰에 파일을 촤르륵 뿌려줘!
-                    FillListView(fbd.SelectedPath, lvFilesLeft);
+                    PopulateListView(fbd.SelectedPath, lvFilesLeft);
                 }
             }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -114,6 +224,16 @@ namespace FileCompare
         }
 
         private void splitContainer1_Panel1_Paint_2(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lvFilesRight_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }

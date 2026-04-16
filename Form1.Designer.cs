969d3fb560d33070c9555fcce12ff8accbc0d1d2
허태유 <gjxodb1458@gmail.com>
2026-04-16 12:56:17 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace FileCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button3 = new Button();
+            button5 = new Button();
+            button2 = new Button();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            listView2 = new ListView();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(listView1);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 9);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(428, 429);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(listView2);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(button3);
+            panel2.Location = new Point(446, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(434, 429);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label1.Location = new Point(19, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 60);
+            label1.TabIndex = 0;
+            label1.Text = "FileCopare";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(277, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 95);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(261, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(286, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 29);
+            button1.TabIndex = 2;
+            button1.Text = "폴더선택";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(279, 48);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 29);
+            button3.TabIndex = 4;
+            button3.Text = "<<<";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(286, 48);
+            button5.Name = "button5";
+            button5.Size = new Size(92, 29);
+            button5.TabIndex = 6;
+            button5.Text = ">>>";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(279, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(92, 29);
+            button2.TabIndex = 5;
+            button2.Text = "폴더선택";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(0, 126);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(428, 300);
+            listView1.TabIndex = 7;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "이름";
+            columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "크기";
+            columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "수정";
+            columnHeader3.Width = 150;
+            // 
+            // listView2
+            // 
+            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader4, columnHeader5, columnHeader6 });
+            listView2.FullRowSelect = true;
+            listView2.GridLines = true;
+            listView2.Location = new Point(0, 123);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(428, 300);
+            listView2.TabIndex = 8;
+            listView2.UseCompatibleStateImageBehavior = false;
+            listView2.View = View.Details;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "이름";
+            columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "크기";
+            columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "수정";
+            columnHeader6.Width = 150;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(945, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button5;
+        private Button button3;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private Button button2;
+        private ListView listView2;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+    }
+}
